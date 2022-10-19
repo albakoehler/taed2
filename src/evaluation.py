791772b3a349d9ee519f9bd940344f81f0dedc74
Alba Koehler <albakoehler@10-192-186-71client.eduroam.upc.edu>
2022-10-19 10:23:51 +0200

    eval changes2

--- src/evaluation.py
+++ src/evaluation.py
@@ -35,8 +35,9 @@ x_test.text= x_test.text.astype(str)
 MAX_SEQUENCE_LENGTH = 30
 x_test = pad_sequences(tokenizer.texts_to_sequences(x_test.text), maxlen = MAX_SEQUENCE_LENGTH)
 
-"""give the sentiment evaluation based on the score"""
+
 def decode_sentiment(score):
+    """give the sentiment evaluation based on the score"""
     return "Positive" if score>0.5 else "Negative"
 
 # Load the model
